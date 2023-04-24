6ee83c62dd841329f690d41761357c463143a9f6
Viktor Krstev <krstevviktor996@gmail.com>
2023-04-24 20:19:18 +0200

    add SearchView

--- app/src/main/java/com/example/barkodershopapp/presentation/Adapters/SelectProductAdapter.kt
+++ app/src/main/java/com/example/barkodershopapp/presentation/Adapters/SelectProductAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.barkodershopapp.presentation.Adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.barkoder.shoppingApp.net.databinding.ActivitySelectProductBinding
 import com.barkoder.shoppingApp.net.databinding.ProductListItemBinding
@@ -11,9 +14,22 @@ import com.example.barkodershopapp.domain.userdataacc.ApiData
 import com.example.barkodershopapp.domain.userdataacc.Product
 import com.example.barkodershopapp.presentation.OnClickSelectProduct
 import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.ArrayList
 
-class SelectProductAdapter(private var list : List<Product>, private val listenerSelect : OnClickSelectProduct ) : RecyclerView.Adapter<SelectProductAdapter.ViewHolder>(){
+class SelectProductAdapter(private var list : ArrayList<Product>, private val listenerSelect : OnClickSelectProduct ) : RecyclerView.Adapter<SelectProductAdapter.ViewHolder>(), Filterable{
 
+    private var productListFull: List<Product> = ArrayList()
+
+    init {
+        productListFull = list
+    }
+
+    fun setProductsList2(productsList: ArrayList<Product>) {
+        productListFull = productsList
+        list = productsList
+        notifyDataSetChanged()
+    }
             class ViewHolder(private val binding : SelectproductItemBinding, private val listenerSelect : OnClickSelectProduct) : RecyclerView.ViewHolder(binding.root) {
                 fun bind (list : Product) {
                     binding.textSelectProductName.text = list.name
@@ -37,9 +53,40 @@ class SelectProductAdapter(private var list : List<Product>, private val listene
         holder.bind(list[position])
     }
 
-    fun setProductsList(lista : List<Product>) {
+
+
+    fun setProductsList(lista : ArrayList<Product>) {
         this.list = lista
         notifyDataSetChanged()
     }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filteredList = ArrayList<Product>()
+                if (constraint == null || constraint.isEmpty()) {
+                    filteredList.addAll(productListFull)
+                } else {
+                    val filterPattern = constraint.toString().toLowerCase(Locale.ROOT).trim()
+                    for (product in productListFull) {
+                        if (product.name.toLowerCase(Locale.ROOT).contains(filterPattern)) {
+                            filteredList.add(product)
+                        }
+                    }
+                }
+                val results = FilterResults()
+                results.values = filteredList
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                list.clear()
+                list.addAll(results?.values as ArrayList<Product>)
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/example/barkodershopapp/presentation/SelectProductActivity.kt
+++ app/src/main/java/com/example/barkodershopapp/presentation/SelectProductActivity.kt
@@ -1,8 +1,10 @@
 package com.example.barkodershopapp.presentation
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.barkoder.shoppingApp.net.R
@@ -14,12 +16,17 @@ import com.example.barkodershopapp.presentation.Adapters.SelectProductAdapter
 import com.example.barkodershopapp.presentation.viewmodel.ProductApiViewModel
 import com.example.barkodershopapp.presentation.viewmodel.ProductViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.internal.notifyAll
+import java.util.*
+import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class SelectProductActivity : AppCompatActivity() {
     lateinit var binding : ActivitySelectProductBinding
     val productApiViewModel : ProductApiViewModel by viewModels()
     val productViewModel : ProductViewModel by viewModels()
+    lateinit var pArrayList : ArrayList<Product>
+    lateinit var nArrayList : ArrayList<Product>
     lateinit var selectAdapter : SelectProductAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +34,10 @@ class SelectProductActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        var productS = arrayListOf<Product>()
+        var productS = ArrayList<Product>()
         selectAdapter = SelectProductAdapter(productS, selectProductBtn)
+
+        selectAdapter.setProductsList2(productS)
 
         binding.recViewSelect.apply {
             layoutManager = LinearLayoutManager(this@SelectProductActivity)
@@ -36,10 +45,45 @@ class SelectProductActivity : AppCompatActivity() {
         }
         productApiViewModel.getAllProductsApi()
 
-        productApiViewModel.resp.observe(this, {products ->
-            selectAdapter.setProductsList(products.products)
+        productApiViewModel.resp.observe(this, { products ->
+            selectAdapter.setProductsList2(products.products as ArrayList<Product>)
+            searchView(products.products)
         })
+
+
+
+
+
+
+
     }
+
+        private fun searchView(list : List<Product>) {
+            binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText.isNullOrEmpty()) {
+                        selectAdapter.setProductsList(list as ArrayList<Product>)
+                    } else {
+                        val filteredList = list.filter { product ->
+                            product.name.contains(newText, ignoreCase = true)
+                        } as ArrayList<Product>
+                        selectAdapter.setProductsList(filteredList)
+                    }
+                    return false
+                }
+
+            })
+        }
+
+
+
+
+
+
 
     private var selectProductBtn = object : OnClickSelectProduct {
         override fun onClickSelect(list: Product) {
