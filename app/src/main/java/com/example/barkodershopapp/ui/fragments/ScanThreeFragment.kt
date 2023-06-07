9ca2fc929f8c6263798bfaa241ca35ecb80ece39
Viktor Krstev <krstevviktor996@gmail.com>
2023-06-07 18:19:57 +0200

    fix bug!

--- app/src/main/java/com/example/barkodershopapp/ui/fragments/ScanThreeFragment.kt
+++ app/src/main/java/com/example/barkodershopapp/ui/fragments/ScanThreeFragment.kt
@@ -45,7 +45,6 @@ class ScanThreeFragment : Fragment(), BarkoderResultCallback {
         )
         {
             Log.i("LicenseInfo", it.message)
-            Log.i("viktor", Barkoder.GetVersion())
         }
 
         if (checkCameraPermission()) {
